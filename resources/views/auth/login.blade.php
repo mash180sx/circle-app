046519d449d00d3b97dfaf517dfe1f9fadf655c3
Masa Shio <mash180sx@gmail.com>
2023-07-20 22:11:19 +0900

    Sign in/up関連の修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,6 +43,14 @@
                     {{ __('Log in') }}
                 </x-button>
             </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('register'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                        {{ __('Sign up') }}
+                    </a>
+                @endif
+            </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>

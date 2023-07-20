1ea27dfb6323cd58697fe61ed2992eedea9aa982
Masa Shio <mash180sx@gmail.com>
2023-07-21 04:44:08 +0900

    update ci sign in

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,23 +25,17 @@
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
             </div>
 
             <div class="flex items-center justify-end mt-4">
@@ -50,6 +44,10 @@
                         {{ __('Sign up') }}
                     </a>
                 @endif
+
+                <x-button class="ml-4">
+                    {{ __('Log in') }}
+                </x-button>
             </div>
         </form>
     </x-authentication-card>

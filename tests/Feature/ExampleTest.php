f2634642bad714fccf1645d7659fb4e55143a0c8
Masa Shio <mash180sx@gmail.com>
2023-07-22 21:41:48 +0900

    modify ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // リダイレクトするように変更済みのため200から302に変更
+        $response->assertStatus(302);
     }
 }
